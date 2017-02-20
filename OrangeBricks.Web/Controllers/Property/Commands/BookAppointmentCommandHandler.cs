f0b1adc5a917e6229bf8e8651b2665f7c7e849c9
D0tNetGeek <vmalvi2003@yahoo.com>
2017-02-20 00:53:03 +0000

    Updated book appointment handle to add appointment

--- OrangeBricks.Web/Controllers/Property/Commands/BookAppointmentCommandHandler.cs
+++ OrangeBricks.Web/Controllers/Property/Commands/BookAppointmentCommandHandler.cs
@@ -11,19 +11,31 @@ namespace OrangeBricks.Web.Controllers.Property.Commands
             _context = context;
         }
 
-        public void Handle(BookAppointmentCommand command)
+        public bool Handle(BookAppointmentCommand command)
         {
+            var property = _context.Properties.Find(command.PropertyId);
+
+            //if (property.Appointments != null && property.Appointments.Count(o => o.BuyerUserId == command.BuyerUserId) > 0) { return false; }
+
             var appointment = new Appointment
             {
                 PropertyId = command.PropertyId,
                 ViewingDateTime = command.ViewingDateTime,
                 BuyerUserId = command.BuyerUserId,
-                IsViewing = true
+                IsViewing = true,
+                UpdatedAt = DateTime.Now
             };
 
-            _context.Appointments.Add(appointment);
+            if (property.Appointments == null)
+            {
+                property.Appointments = new List<Appointment>();
+            }
+
+            property.Appointments.Add(appointment);
 
             _context.SaveChanges();
+
+            return true;
         }
     }
 }
